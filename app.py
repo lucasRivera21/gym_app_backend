cdd9d47290afeaeca9f301d95bf67cb141a6cb7b
lucasRivera21 <lucasriverar2001@gmail.com>
2023-11-07 20:05:05 -0500

    corsmiddleware add

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from routes import users, weights, routines, exercise_user, exercise, login
 from sqlalchemy.orm import Session
 from db import SessionLocal, engine, Base
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
 
+origins = [config('FRONTEND_URL')]
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(users.user)
 app.include_router(weights.weight)
